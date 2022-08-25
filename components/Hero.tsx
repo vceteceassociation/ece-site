8087386fdb2854033a3f42efe1f475f36d9a8808
Mel28vin <jebasamuel@gmail.com>
2022-08-25 22:31:27 +0530

    Font weight change

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -4,7 +4,7 @@ const Hero = () => {
       <div className="hero-overlay bg-opacity-60" />
       <div className="hero-content text-center text-neutral-content max-w-md">
         <div className="max-w-md flex flex-col justify-evenly gap-y-6 my-8 md:gap-y-12 md:my-16">
-          <h1 className="text-3xl sm:text-5xl font-bold font-head">
+          <h1 className="text-3xl sm:text-5xl font-head">
             The ECE Association
           </h1>
           <p className="h-20 sm:h-40 font-body">
