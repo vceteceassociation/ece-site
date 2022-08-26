965ba3b204d453d877d43917ab044eb2d2465a7b
Mel28vin <jebasamuel@gmail.com>
2022-08-26 23:21:18 +0530

    HoD's Desk, Vision and Mission. More UI Optimisations

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,15 +1,11 @@
-import Image from "next/image"
+import Mission from "./Mission"
+import Vision from "./Vision"
 
 const Card = () => {
   return (
-    <div className="card lg:card-side bg-base-100 shadow-xl mt-16 min-w-screen">
-      <div className="card-body">
-        <h2 className="card-title text-2xl">The ECE Association</h2>
-        <p> We are more than a department in VCET. Learn and Grow with us! </p>
-      </div>
-      <figure>
-        <Image src="/ECE-Hero-min.jpeg" width="500" height="300" />
-      </figure>
+    <div className="flex flex-col sm:flex-row justify-evenly mb-6">
+      <Vision />
+      <Mission />
     </div>
   )
 }
---
+++ components/Vision.tsx
@@ -0,0 +1,15 @@
+const Vision = () => {
+  return (
+    <div className="card w-11/12 sm:w-5/12 bg-base-100 shadow-xl mb-4">
+      <div className="card-body">
+        <h2 className="card-title font-head text-center">Our Vision</h2>
+        <p>
+          To emerge as a vibrant centre of repute, moulding students to excel in
+          Electronics and Communication Engineering with ethical responsibility.
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default Vision
